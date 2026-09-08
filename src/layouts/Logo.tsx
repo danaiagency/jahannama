ab8486a9dbdf507678e304e29c0b5e307deafb31
MTR <4mtra5@gmail.com>
2026-09-08 23:45:30 +0330

    اصلاح مسیر Logo.tsx و افزودن images.ts

--- src/layouts/Logo.tsx
+++
@@ -1,5 +0,0 @@
-import { logoImage } from "@/data/images";
-
-export default function Logo({ dark = false }: { dark?: boolean }) {
-  return <img src={logoImage(dark)} alt="جهان‌نما" className="h-8 w-auto" />;
-}
